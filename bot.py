29eac94177e19139e842c84d198b67a361462c91
yasharth45 <62794089+yasharth1@users.noreply.github.com>
2022-01-10 21:16:33 +0530

    Changes

--- bot.py
+++ bot.py
@@ -1,12 +1,5 @@
-import pyttsx3 # pip install pyttsx3
-import winsound
-from num2words import num2words # pip install num2words
 import time
-from datetime import date
-import matplotlib.pyplot as plt # pip install matplotlib
-import cv2 as cv # pip install opencv-python
 import discord
-import asyncio
 from discord.ext import commands
 import time
 intents = discord.Intents.default()
@@ -33,6 +26,8 @@ async def on_message(message):
         await message.add_reaction('👋')
     elif message.content.startswith('prefix'):
         await message.reply("=")
+    elif 'heil hitler' in message.content.lower():
+        await message.reply('The German penal code prohibits publicly denying the Holocaust and disseminating Nazi propaganda, both off- and online. This includes sharing images such as swastikas, wearing an SS uniform and making statements in support of Hitler.')
     elif 'hitler' in message.content.lower():
         await message.reply("Who? That Fascist Fuhrer of Germany? He's the worst. Responsible for killing 7 million Jews!")
     elif message.content.startswith('hi'):
@@ -75,26 +70,36 @@ async def kick(ctx, member: discord.Member, *, reason='{reason}'):
     msg = f"{member} has been kicked because {reason}. And don't worry, I am not sad"
     await ctx.send(msg)
     await member.create_dm.send(msg)
+@bot.command()
+async def mute(ctx, member: discord.Member, *, reason='{reason}'):
+    role = discord.utils.get(ctx.guild.roles, name="muted")
+    await member.add_roles(role)
+    msg = f"**{member}**has been muted because {reason}. The person who muted him be like-"
+    await ctx.send(msg)
+    await ctx.send(file=discord.File('fail.gif'))
+    await member.create_dm.send(msg)
+@bot.command()
+async def unmute(ctx, member: discord.Member):
+    role = discord.utils.get(ctx.guild.roles, name="muted")
+    await member.remove_roles(role)
+    msg = f"{member} has been unmuted."
+    await ctx.send(msg)
+    await member.create_dm.send(msg)
 # The WW2 show begins
 @bot.command()
 async def ww2(ctx, battle_name="None"):
-    try:
-        if battle_name.lower() == "bob":
-          embed = discord.Embed(
-            title='YG Bot Help',
-            colour = discord.Colour.orange()
-          )
-          embed.add_field(name='Battle of Britain', value="Battle of Britain was a full-on Air force battle between Nazi Germany's Luftwaffe and Great Britain's Royal Air force. The RAF won this battle, but many civilians were killed in Luftwaffe Bombing raids.\n After the fall of France, Adolf Hitler wanted to invade Britain, but before that, he needed air and naval superiority!\n So, the Luftwaffe started attacking RAF bases and runways, and inflicted a huge amount of damage. As a strategic plan, Churchill ordered a bombing raid over Berlin, pretty insignificant, but Hitler was furious. He ordered the Luftwaffe to focus its attacks on civilian targets.\nThis led to bombing raids over London and other cities, but gave time to the RAF to rebuild its bases.\nThe Luftwaffe launched a full-blown attack on what is known as 'Battle of Britain day' and the RAF successfully repelled it, placing air superiority in British Hands./n**Hitler's invasion had to be postponed.**", inline=False)
-          await ctx.send("")
-        elif battle_name.lower() == 'tov':
-            await ctx.send("Treaty of Versailles was a treaty between the Allies and Germany after WW1 and one of the main reasoms for the rise of Hitler. Run '=tov terms' to know about the terms of the treaty!")
-    except battle_name == "None":
-        await ctx.send("Missing Attribute!")
+  if battle_name.lower() == "bob":
+    embed = discord.Embed(
+      title='World War 2',
+      colour = discord.Colour.orange()
+    )
+    embed.add_field(name='__Battle of Britain__', value="Battle of Britain was a full-on Air force battle between Nazi Germany's Luftwaffe and Great Britain's Royal Air force. The RAF won this battle, but many civilians were killed in Luftwaffe Bombing raids.\n After the fall of France, Adolf Hitler wanted to invade Britain, but before that, he needed air and naval superiority!\n So, the Luftwaffe started attacking RAF bases and runways, and inflicted a huge amount of damage. As a strategic plan, Churchill ordered a bombing raid over Berlin, pretty insignificant, but Hitler was furious. He ordered the Luftwaffe to focus its attacks on civilian targets.\nThis led to bombing raids over London and other cities, but gave time to the RAF to rebuild its bases.\nThe Luftwaffe launched a full-blown attack on what is known as 'Battle of Britain day' and the RAF successfully repelled it, placing air superiority in British Hands.\n**Hitler's invasion had to be postponed.**", inline=False)
+    await ctx.send(embed=embed)
+  elif battle_name.lower() == 'tov':
+      await ctx.send("Treaty of Versailles was a treaty between the Allies and Germany after WW1 and one of the main reasoms for the rise of Hitler. Run '=tov terms' to know about the terms of the treaty!")
 @bot.command()
-
-@bot.command()
-async def tov(ctx, terms):
+async def tov(ctx, terms=None):
     if terms == "terms":
         await ctx.send("1) Germany can't have an Air Force \n2) It can't have an army of more than a **100,000 men**\n3) The surrender of all German colonies\n4) The return of **Alsace-Lorraine** to France\n5) Cession of some other territories to **Belgium, Lithuania, Czechoslovakia and Poland**\n6) Danzig to become a free city\n7) Plebiscites to be held in northern Schleswig to settle the Danish-German frontier.\n8) Occupation and special status for the **Saar Land** under French control\n9) Demilitarization of the **Rhineland**\n10) Germany would have to pay all war reparations to the allies\n11) **Austria** was taken from Germany and made a seperate country\n12) Germany has to take full responsibity for starting the War\n13) Provision for the trial of the former Kaiser and other war leaders.\nGermany felt extremely humiliated by all this, and nobody was madder about the treaty than Hitler. He made fiery speeches against it which rose him to prominence ")
         await ctx.send(file=discord.File('map.png'))
-bot.run('ODk1NTU4MDI0NjI0NzM4MzU1.YV6TZw.IHA77N1d8xeCU_SWXRMlqt_9e44')
+bot.run('ODk1NTU4MDI0NjI0NzM4MzU1.YV6TZw.bHmbu8UqiYNuTnS627umihldq7A')
